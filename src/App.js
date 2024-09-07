cf20ab675e2990c0c3c451272cce82405a23c14b
islamahmad11 <islamahmad1145@gmail.com>
2024-09-07 15:11:45 +0300

    upload the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useRef, useEffect, useState } from "react";
+import { motion } from "framer-motion"
+import imgs from "./imgs";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const carouselRef = useRef();
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth( carouselRef.current.scrollWidth - carouselRef.current.offsetWidth);
+  }, []);
+  return(
+    <div>
+      <motion.div ref={carouselRef} className="carousel" whileTap={{cursor:"grabbing"}}>
+        <motion.div drag="x" dragConstraints={{ right:0, left: -width }} className="inner-carousel">
+          {
+            imgs.map((image) => (
+              <motion.div className="item" key={image}>
+                <img src={image} alt="carousel-img"/>
+              </motion.div>
+           ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
-}
+};
 
 export default App;
